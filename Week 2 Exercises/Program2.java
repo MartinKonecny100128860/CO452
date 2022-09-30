694bce139e73d0cc6d38262e781e00ef2bb4106a
MartinKonecny100128860 <100128860@student.burnley.ac.uk>
2022-09-30 11:44:35 +0100

    Excercise3MK

--- Week 2 Exercises/Program2.java
+++ Week 2 Exercises/Program2.java
@@ -1,13 +1,16 @@
 import java.util.Scanner;
 
-class Main {
-  public static void main(String[] args) {
+class Program2 
+{
+  public static void main(String[] args) 
+  {
     Scanner myObj = new Scanner(System.in);
     String userName;
     
     System.out.println("Enter username"); 
     userName = myObj.nextLine();   
        
-    System.out.printn("Username is: " + userName);        
+    System.out.println("Username is: " + userName);
+    myObj.close();
   }
 }
---
+++ Week 2 Exercises/Program3.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Main 
+{
+  public static void main(String[] args) 
+  {
+    Scanner myObj = new Scanner(System.in);
+
+    System.out.println("Enter length of rectangle");
+    int length = myObj.nextInt();
+    System.out.println("Enter height of rectangle"); 
+    int height = myObj.nextInt();
+
+    System.out.println("Area= " + length*height); 
+  }  
+}
