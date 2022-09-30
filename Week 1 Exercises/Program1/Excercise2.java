9b227ad7a0c5fee70bd6a1458f0019afc9d68f3a
MartinKonecny100128860 <100128860@student.burnley.ac.uk>
2022-09-30 10:10:04 +0100

    MK Excercise2

---
+++ Week 1 Exercises/Program1/Excercise2.java
@@ -0,0 +1,11 @@
+package Program1;
+class Main
+{
+    public static void main(String[] args)
+    {
+      System.out.println("Hello, what is your name?"); 
+      name = InputReader.getString();
+      System.out.println("Hello " + name + "!"); 
+    }
+
+}
